db1d9188ed219074cccbbb370a2224c6df53ce2c
psycho-pomp <52618538+psycho-pomp@users.noreply.github.com>
2020-11-29 21:19:15 +0530

    Array Roatation by Juggling Algorithm

---
+++ Week2/Array/Learn/Juggling.py
@@ -0,0 +1,27 @@
+# Abhishek Anand
+# Array Rotation-Juggling Algorithm
+def gcd(a,b):
+    while b:
+        a,b=b,a%b
+    return a
+def rotateArray(arr,n,d):
+    d%=n
+    g=gcd(d,n)
+    for i in range(g):
+        temp=arr[i]
+        j=i
+        while(1):
+            k=j+d
+            if k>=n:
+                k-=n
+            if k==i:
+                break
+            arr[j]=arr[k]
+            j=k
+        arr[j]=temp
+    return arr
+    
+n=int(input())
+a=list(map(int,input().split()))
+d=int(input())
+print(rotateArray(a,n,d))
