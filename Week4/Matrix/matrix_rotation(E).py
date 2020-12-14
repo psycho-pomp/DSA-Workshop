8a761716d553826a0be1305c2e8a21aca5be52c7
psycho-pomp <52618538+psycho-pomp@users.noreply.github.com>
2020-12-14 21:44:37 +0530

    Matrix Roataion with Extra Space

---
+++ Week4/Matrix/matrix_rotation(E).py
@@ -0,0 +1,24 @@
+def print_matrix(m,r,c):
+    for i in range(r):
+        for j in range(c):
+            print(m[i][j],end=" ")
+        print()
+def matrix_rotation(a,r,c):
+    temp=[]
+    for i in range(r):
+        x=[]
+        for j in range(c):
+            x.append(a[i][j])
+        temp.append(x)
+    
+    for i in range(r):
+        for j in range(c):
+            a[i][j]=temp[j][c-1-i]
+            
+    
+m,n=map(int,input().split())
+a=[]
+for i in range(m):
+    a.append(list(map(int,input().split())))
+matrix_rotation(a,m,n)
+print_matrix(a,m,n)
