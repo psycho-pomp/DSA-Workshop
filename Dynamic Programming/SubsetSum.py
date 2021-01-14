97f106c74b973e0ff3785b829587fa08c6a2b105
psycho-pomp <52618538+psycho-pomp@users.noreply.github.com>
2021-01-14 16:02:17 +0530

    Subset Sum Problem

---
+++ Dynamic Programming/SubsetSum.py
@@ -0,0 +1,23 @@
+
+        
+
+n=int(input())
+arr=list(map(int,input().split()))
+req_sum=int(input())
+t=[[False for j in range(req_sum+1)] for i in range(n+1)]
+for i in range(n+1):
+    t[i][0]=True
+    
+def knapsack(arr,req_sum,n):
+    
+    for i in range(1,n+1):
+        for j in range(1,req_sum+1):
+            if arr[i-1]<=j:
+                t[i][j]=t[i-1][j-arr[i-1]] or t[i-1][j]
+            else:
+                t[i][j]=t[i-1][j]
+    return t[n][req_sum]
+
+print(knapsack(arr,req_sum,n))
+
+        
